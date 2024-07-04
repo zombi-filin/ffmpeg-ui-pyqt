a6bcc011bded745fdf464bd34211ce783b52478c
zombi-filin <zombi.filin.256@gmail.com>
2024-07-05 00:10:09 +0300

    Переделка на QFormLayout

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('FFmpeg PyQT')
+        self.setFixedWidth(800)
+        self.setFixedHeight(600)
         
-        
-        self.target_label = QLabel(parent = self, text = 'Path')
+        self.form_layout = QFormLayout()
+
+        self.hor_layout_target = QHBoxLayout()
+        self.target_label = QLabel(parent = self, text = 'Файл')
+        self.hor_layout_target.addWidget(self.target_label)
+
         self.target_file_name = QLineEdit(parent = self)
-        
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.target_label, 0 , 0)
-        self.layout.addWidget(self.target_file_name, 0 , 1)
+        self.hor_layout_target.addWidget(self.target_file_name)
+
+        self.form_layout.addRow(self.hor_layout_target)
 
         container = QWidget()
-        container.setLayout(self.layout)
+        container.setLayout(self.form_layout)
+        
         self.setCentralWidget(container)
         
 
