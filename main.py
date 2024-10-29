d4e579ea7ab717018fd826ac7d064f7625e30128
zombi-filin <zombi.filin.256@gmail.com>
2024-10-30 00:12:16 +0300

    Правки 2024.10.29

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import *
 
+import subprocess
 import sys
+
+version = '0.0.1'
 
 # Класс основного меню
 class MainWindow(QMainWindow):
@@ -11,41 +14,64 @@ class MainWindow(QMainWindow):
     # region Init
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('FFmpeg PyQT')
+        self.setWindowTitle(f'FFmpeg PyQT {version}')
         self.setFixedWidth(800)
         self.setFixedHeight(600)
         
         # region Target
-        self.target_file_name = QLineEdit(parent = self)
-        self.target_open_button = QPushButton(text = 'Открыть', parent = self)
-        self.target_open_button.clicked.connect(self.target_open_button_click)
+        self.input_file_width = 0
+        self.input_file_height = 0
+        
+        self.form_edit_target_file_name = QLineEdit(parent=self)
+        self.form_button_target_open = QPushButton(text='Открыть', parent=self)
+        self.form_button_target_open.clicked.connect(self.form_button_target_open_click)
 
-        self.target_hor_layout = QHBoxLayout()
-        self.target_hor_layout.addWidget(self.target_file_name)
-        self.target_hor_layout.addWidget(self.target_open_button)
+        self.form_layout_target = QHBoxLayout()
+        self.form_layout_target.addWidget(self.form_edit_target_file_name)
+        self.form_layout_target.addWidget(self.form_button_target_open)
 
-        self.target_group_box = QGroupBox('Файл')
-        self.target_group_box.setLayout(self.target_hor_layout)
+        self.form_group_box_target = QGroupBox('Файл')
+        self.form_group_box_target.setLayout(self.form_layout_target)
         # endregion Target
+
+        # region Изменение размера
+        self.form_label_resize_width = QLabel(parent=self, text='Ширина')
+        self.form_edit_source_width = QLineEdit(parent=self, readOnly=True)
+
+        self.form_label_resize_height = QLabel(parent=self, text='Высота')
+        self.form_edit_source_height = QLineEdit(parent=self, readOnly=True)
+
+        self.form_layout_resize = QHBoxLayout()
+        self.form_layout_resize.addWidget(self.form_label_resize_width)
+        self.form_layout_resize.addWidget(self.form_edit_source_width)
+        self.form_layout_resize.addWidget(self.form_label_resize_height)
+        self.form_layout_resize.addWidget(self.form_edit_source_height)
+        
+        self.form_group_box_resize = QGroupBox('Изменение размера изображения')
+        self.form_group_box_resize.setCheckable(True)
+        self.form_group_box_resize.setLayout(self.form_layout_resize)
+        # endregion Resize
 
         # region Mainform
         self.form_layout = QFormLayout()
-        self.form_layout.addRow(self.target_group_box)
-        #endregion Mainform
+        self.form_layout.addRow(self.form_group_box_target)
+        self.form_layout.addRow(self.form_group_box_resize)
+        # endregion Mainform
 
         # region Container 
         container = QWidget()
         container.setLayout(self.form_layout)
-        
         self.setCentralWidget(container)
         # endregion Container
 
     # endregion Init
 
     # region Functions
-    def target_open_button_click(self):
+    def form_button_target_open_click(self):
         file_name = QFileDialog.getOpenFileName(self, 'Файл для конвертации', '/', 'Видео файл (*.avi *.mov *.mp4 *.m4a *.3gp *.3g2 *.mj2 *.mpeg)')
         self.target_file_name.setText(file_name[0])
+        cmd = f'ffprobe -v error -show_entries stream=width,height -of default=noprint_wrappers=1:nokey=1 -i {file_name[0]}'
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         pass
     # endregion Functions
 
